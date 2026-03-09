934bf7139ccad9cda0e59d47d6b046d02e967fbe
Velimir Majstorov <majstorov@gmail.com>
2026-03-09 12:01:53 +0100

    Fix duplicate variable declaration in translations index
    Change duplicate 'it' to proper 'id' for Indonesian language import

--- src/i18n/translations/index.ts
+++ src/i18n/translations/index.ts
@@ -7,7 +7,6 @@ const en = require('./en.json') as Record<string, string>;
 const fr = require('./fr.json') as Record<string, string>;
 const de = require('./de.json') as Record<string, string>;
 const it = require('./it.json') as Record<string, string>;
-const it = require('./id.json') as Record<string, string>;
 const pt = require('./pt.json') as Record<string, string>;
 const ro = require('./ro.json') as Record<string, string>;
 const ru = require('./ru.json') as Record<string, string>;
